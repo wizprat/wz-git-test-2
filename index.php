4b0048d2dd23cfbd081824586fa544135a67c8f7
wizprat <wizlipratama06@gmail.com>
2022-04-10 02:48:25 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam maxime eveniet laudantium omnis numquam voluptate quae alias facilis ex, sequi tempore blanditiis dolor sapiente enim qui, non corporis reprehenderit ab.</p>
 </body>
 </html>
